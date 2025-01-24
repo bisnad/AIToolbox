939f9659d9fca6feec14e5a2a6b704a90c35ec5f
bisnad <daniel.bisig@zhdk.ch>
2025-01-24 18:05:52 +0100

    fixed broken mmpose installer

--- Installers/mmpose_windows_cuda11.8_install.bat
+++ Installers/mmpose_windows_cuda11.8_install.bat
@@ -2,7 +2,7 @@ REM Installation Script for UMMPose Conda Environment
 REM Windows GPU (CUDA 11.8) Version
 
 set CONDA_PATH=C:\Users\%USERNAME%\anaconda3
-set ENV_NAME=openmmlab_test
+set ENV_NAME=openmmlab
 set PYTHON_VERSION=3.8
 call %CONDA_PATH%\Scripts\activate.bat
 
@@ -17,22 +17,19 @@ call conda activate %ENV_NAME%
 echo Install Packages
 
 echo Install pytorch
-call conda install pytorch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+call conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 
 echo Install spyder
-call conda install spyder
-
-echo Install numpy version 1.26.4
-call conda install numpy==1.26.4
+call conda install spyder -y
 
 echo Install pyqtgraph
-call conda install pyqtgraph
+call conda install pyqtgraph -y
 
 echo Install pyopengl
-call conda install pyopengl
+call conda install pyopengl -y
 
 echo Install pyopengl
-call conda install pyopengl
+call conda install pyopengl -y
 
 echo Install python-osc
 pip install python-osc
@@ -44,10 +41,10 @@ echo Install mmengine
 mim install mmengine
 
 echo Install mmcv
-mim install "mmcv==2.1.0"
+pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
 
 echo Install mmdet
-mim install "mmdet>=3.1.0"
+pip install "mmdet>=3.1.0"
 
 echo Install mmpose
 mim install "mmpose>=1.1.0" --user
